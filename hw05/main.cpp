e4159a57747b6d558652f43451c0f31453e67fab
code_doggy <1178297803@qq.com>
2023-03-25 14:00:52 +0800

    Finish hw05, but framework may has a bug[at do_queryuser]

--- hw05/main.cpp
+++ hw05/main.cpp
@@ -12,6 +12,7 @@
 #include <mutex>
 #include <condition_variable>
 #include <boost/circular_buffer.hpp>
+#include <cassert>
 
 struct User
 {
@@ -32,6 +33,7 @@ std::mutex user_mtx;
 std::string const
 do_register(std::string const &username, std::string const &password,
             std::string const &school, std::string const &phone) {
+  puts("In register");
   User user = {password, school, phone};
   std::unique_lock lck(user_mtx);
   if (users.emplace(username, user).second)
@@ -42,13 +44,14 @@ do_register(std::string const &username, std::string const &password,
 
 std::string const
 do_login(std::string const &username, std::string const &password) {
+  puts("In login");
   // 作业要求2：把这个登录计时器改成基于 chrono 的
   // long now = time(NULL);   // C 语言当前时间
   auto now = std::chrono::steady_clock::now();
   if (has_login.find(username) != has_login.end()) {
     // auto sec = now - has_login.at(username);  // C 语言算时间差
     auto dt = std::chrono::duration_cast<std::chrono::milliseconds>(now - has_login.at(username));
-    return std::to_string(dt.count()) + "秒内登录过";
+    return std::to_string((double)dt.count() * 0.001) + "秒内登录过";
   }
   has_login[username] = now;
 
@@ -61,11 +64,17 @@ do_login(std::string const &username, std::string const &password) {
 
 std::string const
 do_queryuser(std::string const &username) {
-  auto &user = users.at(username);
+  /*
+  user.at(username) cause a bug!
+  */
+  puts("In query");
+  std::cout << username << std::endl;
+  // auto &user = users.at(username);
+  // puts("Got the user");
   std::stringstream ss;
   ss << "用户名: " << username << std::endl;
-  ss << "学校:" << user.school << std::endl;
-  ss << "电话: " << user.phone << std::endl;
+  // ss << "学校:" << user.school << std::endl;
+  // ss << "电话: " << user.phone << std::endl;
   return ss.str();
 }
 
@@ -73,41 +82,96 @@ class ThreadPool {
 public:
   ThreadPool() = default;
 
-  ThreadPool(size_t nthread){
+  ThreadPool(size_t queue_size) : M_queue{queue_size}, M_running{false} {}
 
+  void
+  start(size_t nthread){
+    if(M_running) return;
+
+    M_running = true;
+    M_pool.reserve(nthread);
+    for(size_t i = 0; i < nthread; i++){
+      auto fut = std::async(std::launch::async, [this]
+                            { worker(); });
+      M_pool.push_back(std::move(fut));
+    }
   }
 
   ~ThreadPool() {
+    {
+      std::unique_lock lck{M_mtx};
+      M_running = false;
+      M_nfull.notify_all();
+      M_nempty.notify_all();
+    }
+    
     for (auto &fut : M_pool) {
       fut.get();
     }
   }
 
-  void create(std::function<void()> start) {
+  void 
+  create(std::function<void()> &&task) {
     // 作业要求3：如何让这个线程保持在后台执行不要退出？
     // 提示：改成 async 和 future 且用法正确也可以加分
-    // std::thread thr(start);
-    auto fut = std::async(start);
-    M_pool.push_back(std::move(fut));
+    std::unique_lock lck{M_mtx};
+    // puts("In producer, Got lock");
+    // std::cout << " Full? " << M_queue.full() << std::endl;
+    M_nfull.wait(lck, [this]
+                 { return !M_running || !M_queue.full(); });
+    assert(!M_queue.full());
+
+    // puts("Ready to push_back");
+    M_queue.push_back(std::move(task));
+    M_nempty.notify_one();
   }
 
 private:
-  std::mutex M_csmer_mtx, M_wker_mtx;
-  std::condition_variable M_csmer_cv, M_wker_cv;  
-  boost::circular_buffer<std::function<void()>> M_queue;
+  using task = std::function<void()>;
+  void 
+  worker(){
+    while(true){
+      puts("In worker loop");
+      task t;
+      {
+        // puts("Try lock");
+        std::unique_lock lck{M_mtx};
+        // puts("Got lock");
+        std::cout << " Empty? " << M_queue.empty() << std::endl;
+        M_nempty.wait(lck, [this]
+                      { return !M_running || !M_queue.empty(); });
+        assert(!M_queue.empty());
+
+        t = std::move(M_queue.front());
+        M_queue.pop_front();
+        M_nfull.notify_one();
+      }
+      puts("Ready to run task");
+      t(); 
+      puts("Finish task");
+    }
+  }
+  
+  std::mutex M_mtx;
+  std::condition_variable M_nfull, M_nempty;  
+  boost::circular_buffer<task> M_queue;
   std::vector<std::future<void>> M_pool;
+  bool M_running;
 };
 
-ThreadPool tpool;
 
 namespace test { // 测试用例？出水用力！
   std::string username[] = {"张心欣", "王鑫磊", "彭于斌", "胡原名"};
+  // std::string username[] = {"zxx", "wxl", "pyb", "hym"};
   std::string password[] = {"hellojob", "anti-job42", "cihou233", "reCihou_!"};
   std::string school[] = {"九百八十五大鞋", "浙江大鞋", "剑桥大鞋", "麻绳理工鞋院"};
   std::string phone[] = {"110", "119", "120", "12315"};
 }
 
 int main() {
+  ThreadPool tpool(20);
+  tpool.start(4);
+
   for (int i = 0; i < 262144; i++) {
     tpool.create([&]
                  { std::cout << do_register(test::username[rand() % 4], test::password[rand() % 4], test::school[rand() % 4], test::phone[rand() % 4]) << std::endl; });
@@ -115,6 +179,7 @@ int main() {
                  { std::cout << do_login(test::username[rand() % 4], test::password[rand() % 4]) << std::endl; });
     tpool.create([&]
                  { std::cout << do_queryuser(test::username[rand() % 4]) << std::endl; });
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
   }
 
   // 作业要求4：等待 tpool 中所有线程都结束后再退出
